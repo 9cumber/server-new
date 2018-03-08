16375ee52eb6b12184dc996ee38bf59c3fee3489
a_r_g_v <info@arg.vc>
2018-03-08 18:46:12 +0900

    yapfing

--- cucumber/endpoint.py
+++ cucumber/endpoint.py
@@ -5,9 +5,11 @@ from cucumber.app import create_app
 
 app = create_app()
 
+
 @app.route('/')
 def index():
     return 'hello'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
